67e3efd3e598a1f1236079e9fe99fb31eb3ae25a
tonyyanxuan <tony0620emma@yahoo.com.tw>
2015-11-12 21:31:32 +0800

    Need a 'read' to clear RXNEIE interrupt

--- 07-Threads/stdio.c
+++ 07-Threads/stdio.c
@@ -1,7 +1,14 @@
+#include <stdint.h>
+#include "malloc.h"
+#include "reg.h"
+
 extern void print_str(const char *);
 
 void USART2_handler(void)
 {
+	char *str = malloc(2);
+	str[0] = *(USART2_DR) & 0xff;
+	str[1] = '\0';
+	print_str(str);
 	print_str("USART2 interrupt!!\n");
-	return;
 }
