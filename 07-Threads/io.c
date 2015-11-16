18d16c4656157bb607b436c19ea362cb0c6044da
tonyyanxuan <tony0620emma@yahoo.com.tw>
2015-11-16 15:17:33 +0800

    USART2 registration implementation
    Any thread that wants to get input from USART2 needs to attach its
    thread_id to the USART2_handler

--- 07-Threads/io.c
+++ 07-Threads/io.c
@@ -1,15 +1,34 @@
 #include <stdint.h>
 #include "malloc.h"
+#include "threads.h"
 #include "reg.h"
 
-extern void print_str(const char *);
+static char keyboard_buffer[MAX_KEYBOARD_BUFFER];
+static int  keyboard_buffer_index = 0;
+static int  waiting_task_id = -1;
+
+void USART2_attach(int thread_id)
+{
+	/* TODO:
+	 * Need a queue for the future use,
+	 * but now we only have shell needs input
+	 */
+	waiting_task_id = thread_id;
+}
 
 void USART2_handler(void)
 {
-	char *str = malloc(2);
-	str[0] = *(USART2_DR) & 0xff;
-	str[1] = '\0';
-	print_str(str);
-	print_str("USART2 interrupt!!\n");
-	free(str);
+	/* TODO:
+	 * Need to implement a mutex here 
+	 * prevent critical section from race condition
+	 */
+
+	if (keyboard_buffer_index == MAX_KEYBOARD_BUFFER)
+		keyboard_buffer[keyboard_buffer_index - 1] = *(USART2_DR) & 0xff;
+	else
+		keyboard_buffer[keyboard_buffer_index++]   = *(USART2_DR) & 0xff;
+	
+	/* If there is a task waiting for input, wake it */
+	if (waiting_task_id != -1)
+		thread_wake(waiting_task_id);
 }
